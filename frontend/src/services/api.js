0acc3519e3d3b659bc10ac5811414dcc5d41d79b
Anupthor007 <77157035+Anupthor007@users.noreply.github.com>
2025-12-30 21:08:53 +0530

    fix: use production backend api in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://beyondchats-fullstack-assignment-felu.onrender.com/";
+const API_BASE_URL =
+  "https://beyondchats-fullstack-assignment-felu.onrender.com/api/articles";
 
 export const getArticles = async () => {
   const response = await axios.get(API_BASE_URL);
